1f6b48f9733034f9e407bc7cc0f60b9eae464d1a
marco Almeida <gomerjim@hotmail.com>
2021-07-26 19:01:03 +0100

    Redesign/improve MVVM Pattern apllied - Add new folders (ViewModels and Models) and created the "Word" class for the model and "WordViewModel" for the view model.

---
+++ SuperMarketAPI/Models/Word.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SuperMarketAPI.Models
+{
+    public class Word : INotifyPropertyChanged
+    {
+        public Word(string[] listOfWords)
+        {
+            _name = listOfWords;
+        }
+
+        private string[] _name;
+        public string[] Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region INotifyProperty Changed Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ SuperMarketAPI/Views/Management.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Runtime.CompilerServices;
+using SuperMarketAPI.Data;
+using SuperMarketAPI.ViewModels;
+
+
+namespace SuperMarketAPI
+{
+    /// <summary>
+    /// Interaction logic for Management.xaml
+    /// </summary>
+    public partial class Management : Window
+    {
+        public Management()
+        {
+            InitializeComponent();
+
+            //Teste MVVM
+
+            string word1 = "Nome da Window";
+            string word2 = "Botão 1";
+            string word3 = "Botão 2";
+            string[] arrayOfWords = new string[]{ word1, word2, word3 };
+
+            DataContext = new WordViewModel(arrayOfWords);
+
+            //Teste da base de dados SuperDataBase
+
+            //using (var db = new SuperContext())
+            //{
+            //    var seller = new Seller() { SellerName = "Teste1", User=new User()};
+            //    db.Sellers.Add(seller);
+            //    db.SaveChanges();
+            //}
+        }
+
+    }
+}
