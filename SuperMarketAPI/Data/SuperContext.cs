ef04d67a43a8bb445e9b284cb104c308626e7028
marco Almeida <gomerjim@hotmail.com>
2021-07-26 16:51:11 +0100

    Management Window - Created and defined the global layout, implement of "INotifyPropertyChanged" interface for 2 buttons. Entity Framework - Created "Seller","User" and "SuperContext" class and defined  a new connection string to new DataBase called "SuperDataBase"

---
+++ SuperMarketAPI/Data/SuperContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SuperMarketAPI.Data
+{
+    public class SuperContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Seller>().HasKey(s => s.UserId);
+
+            //Relação one-to-one entre User e Seller
+            modelBuilder.Entity<User>().HasOptional(u => u.Seller).WithRequired(s => s.User);
+        }
+    }
+}
---
+++ SuperMarketAPI/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace SuperMarketAPI.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SuperMarketAPI.Data.SuperContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(SuperMarketAPI.Data.SuperContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
